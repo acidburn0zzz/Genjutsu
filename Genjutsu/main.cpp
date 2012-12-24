21aba69d16333b9bab0ae5236e9289dc0b4e0467
AJ Minich <aj@ajminich.com>
2012-12-23 21:51:55 -0800

    Yay, the box now moves.

--- Genjutsu/main.cpp
+++ Genjutsu/main.cpp
@@ -2,9 +2,42 @@
 
 #include <GLUT/glut.h>
 
+/*
+ * CONSTANTS
+ */
+
+int lastFrameTime = 0;
+
+float boxX = 0.0f;
+
+/*
+ * FUNCTION DEFINITIONS
+ */
+
 void display(void)
 {
+    if (lastFrameTime == 0)
+    {
+        lastFrameTime = glutGet(GLUT_ELAPSED_TIME);
+    }
+ 
+    int now = glutGet(GLUT_ELAPSED_TIME);
+    int elapsedMilliseconds = now - lastFrameTime;
+    float elapsedTime = elapsedMilliseconds / 1000.0f;
+    lastFrameTime = now;
+    
+    int windowWidth = glutGet(GLUT_WINDOW_WIDTH);
+    
+    boxX += 512.0f * elapsedTime;
+    if (boxX > windowWidth)
+    {
+        boxX -= windowWidth;
+    }
+    
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glPushMatrix();
+    glTranslatef(boxX, 0.0f, 0.0f);
     
     glBegin(GL_QUADS);
     glVertex2f(  0.0f,   0.0f);
@@ -12,6 +45,7 @@ void display(void)
     glVertex2f(128.0f, 128.0f);
     glVertex2f(  0.0f, 128.0f);
     glEnd();
+    glPopMatrix();
     
     glutSwapBuffers();
 }
@@ -38,7 +72,7 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
     glutInitWindowSize(640, 480);
     
-    glutCreateWindow("GLUT Program");
+    glutCreateWindow("Genjutsu");
     
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
